00a3bde74762ae0673fbe985b254cdcd6b5b30d3
xemeriusss <lafciberru-208@hotmail.com>
2022-07-27 16:11:48 +0300

    update key names

--- iotignite_mqtt_client/lib/Datas.dart
+++ iotignite_mqtt_client/lib/Datas.dart
@@ -2,22 +2,22 @@ class Datas{
 
   String login;
   int id;
-  String nodeId;
-  String avatarUrl;
-  String gravatarId;
+  String node_id;
+  String avatar_url;
+  String gravatar_id;
   String url;
-  String htmlUrl;
-  String followersUrl;
-  String followingUrl;
-  String gistsUrl;
-  String starredUrl;
-  String subscriptionsUrl;
-  String organizationsUrl;
-  String reposUrl;
-  String eventsUrl;
-  String receivedEventsUrl;
+  String html_url;
+  String followers_url;
+  String following_url;
+  String gists_url;
+  String starred_url;
+  String subscriptions_url;
+  String organizations_url;
+  String repos_url;
+  String events_url;
+  String received_events_url;
   String type;
-  bool siteAdmin;
+  bool site_admin;
   String name;
   dynamic company;
   String blog;
@@ -25,33 +25,33 @@ class Datas{
   dynamic email;
   dynamic hireable;
   dynamic bio;
-  dynamic twitterUsername;
-  int publicRepos;
-  int publicGists;
+  dynamic twitter_username;
+  int public_repos;
+  int public_gists;
   int followers;
   int following;
-  DateTime createdAt;
-  DateTime updatedAt;
+  DateTime created_at;
+  DateTime updated_at;
 
   Datas(
       this.login,
       this.id,
-      this.nodeId,
-      this.avatarUrl,
-      this.gravatarId,
+      this.node_id,
+      this.avatar_url,
+      this.gravatar_id,
       this.url,
-      this.htmlUrl,
-      this.followersUrl,
-      this.followingUrl,
-      this.gistsUrl,
-      this.starredUrl,
-      this.subscriptionsUrl,
-      this.organizationsUrl,
-      this.reposUrl,
-      this.eventsUrl,
-      this.receivedEventsUrl,
+      this.html_url,
+      this.followers_url,
+      this.following_url,
+      this.gists_url,
+      this.starred_url,
+      this.subscriptions_url,
+      this.organizations_url,
+      this.repos_url,
+      this.events_url,
+      this.received_events_url,
       this.type,
-      this.siteAdmin,
+      this.site_admin,
       this.name,
       this.company,
       this.blog,
@@ -59,34 +59,34 @@ class Datas{
       this.email,
       this.hireable,
       this.bio,
-      this.twitterUsername,
-      this.publicRepos,
-      this.publicGists,
+      this.twitter_username,
+      this.public_repos,
+      this.public_gists,
       this.followers,
       this.following,
-      this.createdAt,
-      this.updatedAt );
+      this.created_at,
+      this.updated_at );
 
   factory Datas.fromJson(Map<String, dynamic> json){
     return Datas(
         json["login"] as String,
         json["id"] as int,
-        json["nodeId"] as String,
-        json["avatarUrl"] as String,
-        json["gravatarId"] as String,
+        json["node_id"] as String,
+        json["avatar_url"] as String,
+        json["gravatar_id"] as String,
         json["url"] as String,
-        json["htmlUrl"] as String,
-        json["followersUrl"] as String,
-        json["followingUrl"] as String,
-        json["gistsUrl"] as String,
-        json["starredUrl"] as String,
-        json["subscriptionsUrl"] as String,
-        json["organizationsUrl"] as String,
-        json["reposUrl"] as String,
-        json["eventsUrl"] as String,
-        json["receivedEventsUrl"] as String,
+        json["html_url"] as String,
+        json["followers_url"] as String,
+        json["following_url"] as String,
+        json["gists_url"] as String,
+        json["starred_url"] as String,
+        json["subscriptions_url"] as String,
+        json["organizations_url"] as String,
+        json["repos_url"] as String,
+        json["events_url"] as String,
+        json["received_events_url"] as String,
         json["type"] as String,
-        json["siteAdmin"] as bool,
+        json["site_admin"] as bool,
         json["name"] as String,
         json["company"] as dynamic,
         json["blog"] as String,
@@ -94,13 +94,13 @@ class Datas{
         json["email"] as dynamic,
         json["hireable"] as dynamic,
         json["bio"] as dynamic,
-        json["twitterUsername"] as dynamic,
-        json["publicRepos"] as int,
-        json["publicGists"] as int,
+        json["twitter_username"] as dynamic,
+        json["public_repos"] as int,
+        json["public_gists"] as int,
         json["followers"] as int,
         json["following"] as int,
-        json["createdAt"] as DateTime,
-        json["updatedAt"] as DateTime,
+        json["created_at"] as DateTime,
+        json["updated_at"] as DateTime,
     );
 
   }
